fdce4a3eb651e2b98ae8630127fc263b4b70a276
kat-mart <katmartinez@chapman.edu>
2026-02-28 22:10:47 -0800

    add more images

--- server/show_plants.js
+++ server/show_plants.js
@@ -14,18 +14,18 @@ const db = new sqlite3.Database("./plants.db");
 // });
 
 // example search query
-db.all(
-  "SELECT * FROM plants WHERE plantName LIKE ?",
-  ["%Jasmine%"],
-  (err, rows) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.table(rows);
-    }
-    db.close();
-  }
-);
+// db.all(
+//   "SELECT * FROM plants WHERE plantName LIKE ?",
+//   ["%Jasmine%"],
+//   (err, rows) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.table(rows);
+//     }
+//     db.close();
+//   }
+// );
 
 // Delete rows matching the plant name
 // db.run(
@@ -41,19 +41,41 @@ db.all(
 //   }
 // );
 
-// update image url
+// // update image url
 // db.run(
 //   "UPDATE plant_images SET imageUrl = ? WHERE plantName LIKE ?",
-//   ["https://images.cookforyourlife.org/wp-content/uploads/2018/08/shutterstock_224264125-min.jpg", "%Cilantro%"],
+//   [
+//     "https://www.outsidepride.com/images/products/detail/gardenflower/birdofparadise.jpg", 
+//     "%Bird Of Paradise%" 
+//   ],
 //   function (err) {
 //     if (err) {
 //       console.error(err);
 //     } else {
 //       console.log(`Rows updated: ${this.changes}`);
 //     }
-//     db.close();
+//     db.close(); 
 //   }
 // );
+
+const plant = "Snake Plant";
+const urls = [
+  "https://www.michlers.com/cdn/shop/products/SnakePlant.jpg?v=1675204628&width=2048",
+  "https://images.squarespace-cdn.com/content/v1/54fbb611e4b0d7c1e151d22a/1610074066643-OP8HDJUWUH8T5MHN879K/Snake+Plant.jpg?format=1000w",
+];
+
+// Loop through your URLs and insert them as individual rows
+urls.forEach((url) => {
+  db.run(
+    "INSERT INTO plant_images (plantName, imageUrl) VALUES (?, ?)",
+    [plant, url],
+    function (err) {
+      if (err) console.error(err);
+      else console.log(`Inserted image for ${plant}. Row ID: ${this.lastID}`);
+    }
+  );
+});
+
 
 // db.run("ALTER TABLE plants ADD COLUMN description TEXT;", (err) => {
 //   if (err) {
